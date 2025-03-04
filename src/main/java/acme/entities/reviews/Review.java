9957a03b69829646fbd9976753e24ee77ebae1f4
Rares Nicolae Petre <rarpet@alum.us.es>
2025-03-04 14:12:29 +0100

    Entity Review

---
+++ src/main/java/acme/entities/reviews/Review.java
@@ -0,0 +1,56 @@
+
+package acme.entities.reviews;
+
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+
+public class Review extends AbstractEntity {
+
+	// Serialisation version
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				alias;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				text;
+
+	@Optional
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Double				score;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Boolean				isRecommended;
+
+}
