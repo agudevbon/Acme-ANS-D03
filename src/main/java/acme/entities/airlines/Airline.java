ad269b835b95bbd16c860c8bbfe55148a7d9d6bd
JuanManuelRomeroRuiz <DP2@JUANMA-DESKTOP>
2025-03-11 01:31:38 +0100

    Groupal Mandatory 5 - Airline Entity

---
+++ src/main/java/acme/entities/airlines/Airline.java
@@ -0,0 +1,67 @@
+
+package acme.entities.airlines;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Airline extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Length(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@Column(unique = true)
+	private String				iataCode;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				website;
+
+	@Mandatory
+	@Automapped
+	private AirlineType			type;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				foundationMoment;
+
+	@Optional
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Optional
+	@Pattern(regexp = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+}
