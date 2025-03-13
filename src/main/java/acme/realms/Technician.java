91d5842a96e2ef30915b31cc5c2e870be551ec6e
fabrobher <fabrobher@alum.us.es>
2025-03-13 20:12:43 +0100

    validIdentifier

--- src/main/java/acme/realms/Technician.java
+++ src/main/java/acme/realms/Technician.java
@@ -11,6 +11,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidIdentifier;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,7 +27,7 @@ public class Technician extends AbstractRole {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@ValidIdentifier
 	@Column(unique = true)
 	private String				licenseNumber;
 
