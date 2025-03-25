69fa95e9605705e374ce721e9779ad2c8b92da9b
agudevbon <agudevbon@alum.us.es>
2025-03-25 11:31:12 +0100

    .

---
+++ src/main/java/acme/features/manager/flight/ManagerFlightCreateService.java
@@ -0,0 +1,71 @@
+
+package acme.features.manager.flight;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.flights.Flight;
+import acme.realms.Manager;
+
+@GuiService
+public class ManagerFlightCreateService extends AbstractGuiService<Manager, Flight> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ManagerFlightRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Flight flight;
+		Manager manager;
+
+		manager = (Manager) super.getRequest().getPrincipal().getActiveRealm();
+
+		flight = new Flight();
+		flight.setDraftMode(true);
+		flight.setManager(manager);
+
+		super.getBuffer().addData(flight);
+
+	}
+
+	@Override
+	public void bind(final Flight flight) {
+
+		super.unbindObject(flight, "tag", "indication", "cost", "description");
+	}
+
+	@Override
+	public void validate(final Flight flight) {
+		boolean confirmation;
+
+		confirmation = super.getRequest().getData("confirmation", boolean.class);
+		super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
+	}
+
+	@Override
+	public void perform(final Flight flight) {
+		this.repository.save(flight);
+	}
+
+	@Override
+	public void unbind(final Flight flight) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(flight, "tag", "indication", "cost", "description");
+
+		super.getResponse().addData(dataset);
+	}
+}
