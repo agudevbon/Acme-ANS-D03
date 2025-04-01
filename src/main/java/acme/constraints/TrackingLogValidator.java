2854a25037dc355b3363c99184703b06c3dc6e5d
Rares Nicolae Petre <rarpet@alum.us.es>
2025-04-01 23:41:08 +0200

    fix TrackingLog Validator and Sample Data

--- src/main/java/acme/constraints/TrackingLogValidator.java
+++ src/main/java/acme/constraints/TrackingLogValidator.java
@@ -9,15 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.validation.AbstractValidator;
 import acme.entities.claims.AcceptedStatus;
+import acme.entities.claims.ClaimRepository;
 import acme.entities.trackingLogs.TrackingLog;
 
 public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, TrackingLog> {
 
-//	@Autowired
-//	private ClaimRepository claimRepository;
-	
+	@Autowired
+	private ClaimRepository claimRepository;
+
 	// ConstraintValidator interface
-	
+
+
 	@Override
 	protected void initialise(final ValidTrackingLog annotation) {
 		assert annotation != null;
@@ -25,58 +27,56 @@ public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, Tr
 
 	@Override
 	public boolean isValid(final TrackingLog trackingLog, final ConstraintValidatorContext context) {
-			assert context != null;
+		assert context != null;
 
-			boolean result;
+		boolean result;
 
-			if (trackingLog == null)
-				super.state(context, false, "*", "acme.validation.NotNull.message");
-			else {
+		if (trackingLog == null)
+			super.state(context, false, "*", "acme.validation.NotNull.message");
+		else {
 
-				// Checking resolution attribute when resolution percentage is 100
-				{
-					boolean shouldExistResolution;
-					
-					shouldExistResolution = trackingLog.getResolutionPercentage() != 100.00 || trackingLog.isResolutionValid();
-					super.state(context, shouldExistResolution, "resolution", "acme.validation.trackinLog.shouldExistResolution.message");
-				}
+			// Checking resolution attribute when resolution percentage is 100
+			{
+				boolean shouldExistResolution;
 
-				// Checking accepted status and percentage matches
-				{
-					boolean isLogAcceptedLogic;
-					boolean isPending = trackingLog.getIsAccepted().equals(AcceptedStatus.PENDING);
-					boolean isComplete = trackingLog.getResolutionPercentage() == 100.0;
-
-					isLogAcceptedLogic = !isComplete && isPending || isComplete && !isPending;
-					super.state(context, isLogAcceptedLogic, "isAccepted", "acme.validation.trackingLog.isLogAcceptedLogic.message");
-				}
-
-				// Checking there only exists 1 completed tracking log
-				{
-					boolean checkExistsOnlyOneCompletedTrackingLog;
-					
-					List<TrackingLog> trackingLogs = this.claimRepository.findAllByClaimId(trackingLog.getClaim().getId());
-					trackingLogs = trackingLogs.stream().filter(log -> log.getResolutionPercentage() == 100.00).toList();
-					checkExistsOnlyOneCompletedTrackingLog = trackingLogs.size() <= 2 ? true : false;
-
-					super.state(context, checkExistsOnlyOneCompletedTrackingLog, "checkExistsOnlyOneCompletedTrackingLog", "acme.validation.trackingLog.checkExistsOnlyOneCompletedTrackingLog.message");
-				}
-
-				// Checking if draftMode attribute matches his claim logic
-				{
-					boolean isDraftModeLogic;
-
-					isDraftModeLogic = !trackingLog.isDraftMode() || trackingLog.getClaim().isDraftMode();
-					super.state(context, isDraftModeLogic, "draftMode", "acme.validation.trackingLog.isDraftModeLogic.message");
-
-				}
+				shouldExistResolution = trackingLog.getResolutionPercentage() != 100.00 || trackingLog.isResolutionValid();
+				super.state(context, shouldExistResolution, "resolution", "acme.validation.trackinLog.shouldExistResolution.message");
 			}
 
-			result = !super.hasErrors(context);
+			// Checking accepted status and percentage matches
+			{
+				boolean isLogAcceptedLogic;
+				boolean isPending = trackingLog.getIsAccepted().equals(AcceptedStatus.PENDING);
+				boolean isComplete = trackingLog.getResolutionPercentage() == 100.0;
 
-			return result;
+				isLogAcceptedLogic = !isComplete && isPending || isComplete && !isPending;
+				super.state(context, isLogAcceptedLogic, "isAccepted", "acme.validation.trackingLog.isLogAcceptedLogic.message");
+			}
+
+			// Checking there only exists 1 completed tracking log
+			{
+				boolean checkExistsOnlyOneCompletedTrackingLog;
+
+				List<TrackingLog> trackingLogs = this.claimRepository.findAllByClaimId(trackingLog.getClaim().getId());
+				trackingLogs = trackingLogs.stream().filter(log -> log.getResolutionPercentage() == 100.00).toList();
+				checkExistsOnlyOneCompletedTrackingLog = trackingLogs.size() <= 2 ? true : false;
+
+				super.state(context, checkExistsOnlyOneCompletedTrackingLog, "checkExistsOnlyOneCompletedTrackingLog", "acme.validation.trackingLog.checkExistsOnlyOneCompletedTrackingLog.message");
+			}
+
+			// Checking if draftMode attribute matches his claim logic
+			{
+				boolean isDraftModeLogic;
+
+				isDraftModeLogic = !trackingLog.isDraftMode() || trackingLog.getClaim().isDraftMode();
+				super.state(context, isDraftModeLogic, "draftMode", "acme.validation.trackingLog.isDraftModeLogic.message");
+
+			}
 		}
 
+		result = !super.hasErrors(context);
+
+		return result;
 	}
 
 }
