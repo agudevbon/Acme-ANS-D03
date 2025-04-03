a2d6981ec562129adf39c06896c3aaaf2eeb225b
Rares Nicolae Petre <rarpet@alum.us.es>
2025-04-03 20:59:20 +0200

    AgentClaim Controller

--- src/main/java/acme/features/assistanceAgent/claim/AssistanceAgentClaimController.java
+++ src/main/java/acme/features/assistanceAgent/claim/AssistanceAgentClaimController.java
@@ -18,9 +18,6 @@ public class AssistanceAgentClaimController extends AbstractGuiController<Assist
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	private AssistanceAgentClaimListService				listService;
-
-	@Autowired
 	private AssistanceAgentClaimListUndergoingService	listUndergoingService;
 
 	@Autowired
@@ -46,7 +43,6 @@ public class AssistanceAgentClaimController extends AbstractGuiController<Assist
 
 	@PostConstruct
 	protected void initialise() {
-		super.addBasicCommand("list", this.listService);
 		super.addBasicCommand("show", this.showService);
 		super.addBasicCommand("create", this.createService);
 		super.addBasicCommand("update", this.updateService);
